78e3cceb28f002ae12cdf7a891e7266e520b1d8b
Jayde Dragyn <jaydedragyn@outlook.com>
2026-03-01 03:05:45 -0500

    Add File menu with Quit option that Quits the program.

--- src/JSIV/ProgramWindow.java
+++ src/JSIV/ProgramWindow.java
@@ -1,31 +1,69 @@
 package JSIV;
 
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class ProgramWindow {
 
+    // Actions
+    private Action openAction;
+    private Action openNextAction;
+    private Action openPreviousAction;
+    private Action quitAction;
+    private Action panAction;
+    private Action moveImageAction;
+    private Action zoomInAction;
+    private Action zoomOutAction;
+    private Action resetZoomAction;
+    private Action centerImageAction;
+    private Action userManualAction;
+    private Action aboutAction;
+
+    // Window components
     private JFrame frame;
+    private JMenuBar menuBar;
     
     public ProgramWindow() {
         initActionItems();
-        frame = initFrame();
+        initFrame();
         initMenu();
         initViewport();
         initStatusBar();
-        // add the things
-        // show the window
+
+        frame.setJMenuBar(menuBar);
+        
+        frame.setVisible(true);
     }
 
     private void initActionItems() {
-        
+        quitAction = new AbstractAction("Quit") {
+            {
+                putValue(ACCELERATOR_KEY,
+                    KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_DOWN_MASK));
+                putValue(MNEMONIC_KEY, KeyEvent.VK_Q);
+            }
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        };
     }
     
-    private JFrame initFrame() {
-        return new JFrame("Placeholder");
+    private void initFrame() {
+        frame = new JFrame("JSIV");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
     }
     
     private void initMenu() {
+        menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        fileMenu.add(new JMenuItem(quitAction));
         
+        menuBar.add(fileMenu);
     }
     
     private void initViewport() {
