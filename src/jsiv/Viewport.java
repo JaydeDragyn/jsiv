9e023e2ab4d6c2e9477d925b4612b4d619b868c7
Jayde Dragyn <jaydedragyn@outlook.com>
2026-03-16 00:03:51 -0400

    Add a guard against a null pointer exception if image is null when checking pixel color

--- src/jsiv/Viewport.java
+++ src/jsiv/Viewport.java
@@ -429,9 +429,10 @@ public class Viewport extends JPanel {
                 }
 
                 // if pointer is not over the image, report Black pixel
-                if ((focusPixelX < 0) || (focusPixelY < 0) ||
-                     (focusPixelX >= imageSize.width) ||
-                     (focusPixelY >= imageSize.height)) {
+                if ((focusPixelX < 0) || (focusPixelY < 0)
+                     || (focusPixelX >= imageSize.width)
+                     || (focusPixelY >= imageSize.height))
+                     || (image == null) {
                      viewportListener.newColorUnderPointer(0, 0, 0);
                 } else {
                     // otherwise report color of pixel under pointer
