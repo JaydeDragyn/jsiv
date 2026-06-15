c2a74e4b845980df5bf37977f6af161e3e1bf64f
Jayde Dragyn <jaydedragyn@outlook.com>
2026-06-15 02:23:06 -0400

    Fix build scripts to properly package external dependencies

--- make-Mint.sh
+++ make-Mint.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
-printf "Cleaning...\n"
-printf "    old jsiv.jar\n"
-rm -f jsiv.jar
+echo Making JSIV in Linux Mint
+echo
+echo Cleaning...
 printf "    jar staging directory\n"
 rm -rf jar/
-printf "    jsiv application directory\n"
+printf "    app staging directory\n"
+rm -rf app_staging/
+printf "    old application directory\n"
 rm -rf jsiv/
 
-printf "Compiling...\n"
+echo Compiling...
 javac -d jar/ -cp "src/:libs/*" src/jsiv/*.java src/usermanual/*.java
 if [[ $? -ne 0 ]]; then
     printf "Compilation failed!\n"
     exit 1
 fi
 
-printf "Making executable JAR...\n"
-jar -cfm jsiv.jar manifest.txt -C . *.MD *.txt *.sh src/ -C jar/ . -C . assets/ -C . libs/ -C . docs/
+echo Making executable JAR...
+jar -cfm jsiv.jar manifest.txt -C jar/ .
 if [[ $? != 0 ]]; then
     printf "Could not create JAR!\n"
     exit 1
 fi
 
-printf "Packaging...\n"
-mkdir jsiv_app
-cp jsiv.jar jsiv_app/
+echo Staging files for packaging...
+mkdir app_staging/
+mkdir app_staging/dev/
+
+mv jsiv.jar app_staging/
+
+cp -rf libs/ app_staging/
+cp -rf assets/ app_staging/
+cp -rf mime-types/ app_staging/
+
+cp *.MD app_staging/dev/
+cp make* app_staging/dev/
+cp -rf src/ app_staging/dev/
+cp manifest.txt app_staging/dev/
+
+echo Packaging...
 jpackage --type deb \
          --name jsiv \
          --linux-package-name jsiv-app \
-         --input jsiv_app \
+         --input app_staging \
          --main-jar jsiv.jar \
          --add-modules java.desktop,java.base \
          --linux-shortcut \
@@ -52,6 +67,7 @@ if [[ $? != 0 ]]; then
 fi
 
 printf "Cleaning up staging files...\n"
-rm -rf jsiv_app/
+rm -rf jar/
+rm -rf app_staging/
 
 printf "Done.\n"
--- make.bat
+++ make.bat
@@ -1,12 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo Making JSIV in Windows
+echo.
 echo Cleaning...
-echo     old jsiv.jar
-if exist jsiv.jar del /f /q jsiv.jar
 echo     jar staging directory
 if exist jar\ rmdir /s /q jar\
-echo     jsiv application directory
+echo     app staging directory
+if exist app_staging\ rmdir /s /q app_staging\
+echo     old application directory
 if exist jsiv\ rmdir /s /q jsiv\
 
 echo Compiling...
@@ -17,22 +19,39 @@ if %ERRORLEVEL% neq 0 (
 )
 
 echo Making executable JAR...
-jar -cfm jsiv.jar manifest.txt -C . *.MD *.txt *.bat src\ -C jar\ . -C . assets\ -C . libs\ -C . docs\
+:: Create the executable JAR in the app_staging directory
+jar -cfm jsiv.jar manifest.txt -C jar\ .
 if %ERRORLEVEL% neq 0 (
     echo Error: Could not create JAR!
     exit /b 1
 )
 
+echo Staging files for packaging...
+:: Copy the executable JAR into the app_staging directory
+if not exist app_staging\ mkdir app_staging\
+move jsiv.jar app_staging\
+
+:: Copy the external dependencies and assets into the app_staging directory
+if exist libs\ xcopy /q /e /i /y libs\ app_staging\libs\
+if exist assets\ xcopy /q /e /i /y assets\ app_staging\assets\
+if exist mime-types\ xcopy /q /e /i /y mime-types\ app_staging\mime-types\
+
+:: Copy the project files into a dev\ folder in the app_staging directory
+if not exist app_staging\dev\ mkdir app_staging\dev\
+if exist src\ xcopy /q /e /i /y src\ app_staging\dev\src\
+copy manifest.txt app_staging\dev\
+copy make* app_staging\dev\
+copy *.MD app_staging\dev\
+
 echo Packaging...
-if not exist jsiv_app\ mkdir jsiv_app
-copy jsiv.jar jsiv_app\
-jpackage.exe --type app-image --name jsiv --input jsiv_app --main-jar jsiv.jar --add-modules java.desktop,java.base --icon assets\jsiv.ico
+jpackage --type app-image --name jsiv --input app_staging --main-jar jsiv.jar --add-modules java.desktop,java.base --icon assets\jsiv.ico
 if %ERRORLEVEL% neq 0 (
     echo Error: Could not package executable!
     exit /b 1
 )
 
 echo Cleaning up staging files...
-if exist jsiv_app\ rmdir /s /q jsiv_app\
+if exist jar\ rmdir /s /q jar\
+if exist app_staging\ rmdir /s /q app_staging\
 
 echo Done.
